3dd75edd9aaa9672145da43f70b9070ab20f9480
Elia Toribio Guillén <elitoribio01@gmail.com>
2023-09-07 19:53:51 -0400

    Added touchmove support for mobile and tablet

--- app.js
+++ app.js
@@ -13,26 +13,12 @@ container.addEventListener('mousemove', (e) => {
     const x = e.clientX - container.getBoundingClientRect().left;
     const y = e.clientY - container.getBoundingClientRect().top;
 
-    // const imageX = (x / container.offsetWidth) * image1.width;
-    // const imageY = (y / container.offsetHeight) * image1.height;
-
-    // const imageX2 = (x / container.offsetWidth) * image2.width;
-    // const imageY2 = (y / container.offsetHeight) * image2.height;
-
     const imageX = (x / container.offsetWidth) * (image1.width * scale);
     const imageY = (y / container.offsetHeight) * (image1.height * scale);
-
-    // const imageX2 = (x / container.offsetWidth) * (image2.width * scale);
-    // const imageY2 = (y / container.offsetHeight) * (image2.height * scale);
-
-    // console.log("Este es width de la primera imagen: "+imageX+ " y la segunda "+imageX2)
-    // console.log("Este es height de la primera imagen: "+imageY+ " y la segunda "+imageX2)
-
 
     magnifier.style.backgroundImage = `url('${image2.src}')`;
     magnifier.style.backgroundSize = `${image1.width}px ${image1.height}px`; // Usa el tamaño de la imagen 1
     magnifier.style.backgroundPosition = `-${imageX}px -${imageY}px`;
-    // magnifier.style.backgroundSize = `${image1.width * scale}px ${image1.height * scale}px`;
     
     magnifier.style.display = 'block';
     magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
@@ -48,3 +34,40 @@ container.addEventListener('mouseleave', () => {
     magnifier.style.display = 'none';
 });
 
+
+// Touchmove for mobile and tablet versions
+
+
+container.addEventListener('mousemove', (e) => {
+    updateMagnifier(e.clientX, e.clientY);
+});
+
+container.addEventListener('touchmove', (e) => {
+    if (e.touches.length === 1) {
+        e.preventDefault(); 
+        const touch = e.touches[0];
+        updateMagnifier(touch.clientX, touch.clientY);
+    }
+});
+
+container.addEventListener('mouseenter', () => {
+    magnifier.style.display = 'block';
+});
+
+container.addEventListener('mouseleave', () => {
+    magnifier.style.display = 'none';
+});
+
+function updateMagnifier(x, y) {
+    const containerRect = container.getBoundingClientRect();
+    const imageX = ((x - containerRect.left) / container.offsetWidth) * (image1.width * scale);
+    const imageY = ((y - containerRect.top) / container.offsetHeight) * (image1.height * scale);
+
+    magnifier.style.backgroundImage = `url('${image2.src}')`;
+    magnifier.style.backgroundSize = `${image1.width}px ${image1.height}px`;
+    magnifier.style.backgroundPosition = `-${imageX}px -${imageY}px`;
+
+    magnifier.style.display = 'block';
+    magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
+    magnifier.style.top = `${y - magnifier.offsetHeight / 2}px`;
+}
