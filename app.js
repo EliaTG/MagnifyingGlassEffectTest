ac04cebe84efecf8cf7b51dd19ecd329265370ca
Elia Toribio Guillén <elitoribio01@gmail.com>
2023-09-08 06:55:09 -0400

    Applied 2x zoom level

--- app.js
+++ app.js
@@ -1,90 +1,4 @@
 
-
-// test
-const container = document.querySelector('.container');
-const magnifier = document.getElementById('magnifier');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-
-
-const scale = 0.8;
-
-container.addEventListener('mousemove', (e) => {
-    const x = e.clientX - container.getBoundingClientRect().left;
-    const y = e.clientY - container.getBoundingClientRect().top;
-
-    const imageX = (x / container.offsetWidth) * (image1.width * scale);
-    const imageY = (y / container.offsetHeight) * (image1.height * scale);
-
-    magnifier.style.backgroundImage = `url('${image2.src}')`;
-    magnifier.style.backgroundSize = `${image1.width}px ${image1.height}px`; // Usa el tamaño de la imagen 1
-    magnifier.style.backgroundPosition = `-${imageX}px -${imageY}px`;
-    
-    magnifier.style.display = 'block';
-    magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
-    magnifier.style.top = `${y - magnifier.offsetHeight / 2}px`;
-});
-
-
-container.addEventListener('mouseenter', () => {
-    magnifier.style.display = 'block';
-});
-
-container.addEventListener('mouseleave', () => {
-    magnifier.style.display = 'none';
-});
-
-
-// Touchmove for mobile and tablet versions
-
-container.addEventListener('mousemove', (e) => {
-    updateMagnifier(e.clientX, e.clientY);
-});
-
-container.addEventListener('touchmove', (e) => {
-    if (e.touches.length === 1) {
-        e.preventDefault(); 
-        const touch = e.touches[0];
-        updateMagnifier(touch.clientX, touch.clientY);
-    }
-});
-
-container.addEventListener('mouseenter', () => {
-    magnifier.style.display = 'block';
-});
-
-container.addEventListener('mouseleave', () => {
-    magnifier.style.display = 'none';
-});
-
-function updateMagnifier(x, y) {
-    const containerRect = container.getBoundingClientRect();
-    const imageX = ((x - containerRect.left) / container.offsetWidth) * (image1.width * scale);
-    const imageY = ((y - containerRect.top) / container.offsetHeight) * (image1.height * scale);
-
-    magnifier.style.backgroundImage = `url('${image2.src}')`;
-    magnifier.style.backgroundSize = `${image1.width}px ${image1.height}px`;
-    magnifier.style.backgroundPosition = `-${imageX}px -${imageY}px`;
-
-    magnifier.style.display = 'block';
-    magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
-    magnifier.style.top = `${y - magnifier.offsetHeight / 2}px`;
-}
-
-
-// Greensock animation
-// const cta = document.querySelector(".cta")
-
-// const scaleTween = gsap.to(cta, {scale: 1, repeat: 9, yoyo: true, paused: true});
-
-// cta.addEventListener("mouseenter", ()=> scaleTween.restart())
-
-// cta.addEventListener("mouseleave", ()=> {
-//   scaleTween.pause()
-//   gsap.to(cta, {scale:0.8})
-  
-// })
-
 // Button pulse effect
 document.addEventListener("DOMContentLoaded", () => {
     const cta = document.querySelector(".cta");
@@ -106,3 +20,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
+// DOM manipulation
+const container = document.querySelector('.container');
+const magnifier = document.getElementById('magnifier');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+
+
+let zoomLevel = 2; // Here you can adjust the zoom level.
+
+// Change the size of the second image based on the zoom.
+image2.style.width = `${image1.width * zoomLevel}px`;
+image2.style.height = `${image1.height * zoomLevel}px`;
+
+// Show the magnifier when the cursor enters the container.
+container.addEventListener('mouseenter', () => {
+    magnifier.style.display = 'block';
+});
+
+// Hide the magnifier when the cursor leaves the container.
+container.addEventListener('mouseleave', () => {
+    magnifier.style.display = 'none';
+});
+
+function handleMove(e) {
+    let x, y;
+    
+    if (e.type === 'mousemove') {
+        x = e.clientX - container.getBoundingClientRect().left;
+        y = e.clientY - container.getBoundingClientRect().top;
+    } else if (e.type === 'touchmove') {
+        if (e.touches.length === 1) {
+            e.preventDefault();
+            const touch = e.touches[0];
+            x = touch.clientX - container.getBoundingClientRect().left;
+            y = touch.clientY - container.getBoundingClientRect().top;
+        }
+    }
+
+    if (x !== undefined && y !== undefined) {
+
+        const imageX = (x / container.offsetWidth) * (image1.width * zoomLevel);
+        const imageY = (y / container.offsetHeight) * (image1.height * zoomLevel);
+
+        magnifier.style.backgroundImage = `url('${image2.src}')`;
+        magnifier.style.backgroundSize = `${image2.width}px ${image2.height}px`; 
+        magnifier.style.backgroundPosition = `-${imageX}px -${imageY}px`;
+
+
+        magnifier.style.display = 'block';
+        magnifier.style.left = `${x - magnifier.offsetWidth / 2}px`;
+        magnifier.style.top = `${y - magnifier.offsetHeight / 2}px`;
+    }
+}
+
+container.addEventListener('mousemove', handleMove);
+container.addEventListener('touchmove', handleMove);
+
